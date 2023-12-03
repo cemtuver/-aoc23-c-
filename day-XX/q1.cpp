225cd1be4a3f59268d918bccde36fa093195ac74
Muzaffer Cem Tüver <cemtuver@outlook.com>
2023-12-03 20:22:02 +0100

    Create the template folder

---
+++ day-XX/q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "../lib/Input.hpp"
+
+using namespace std;
+
+int main() {
+    int result = 0;
+
+    for (string line : input::readLines("input.txt")) {
+        
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
